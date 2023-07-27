491623ea2362e0cae27a20098c1e408a35e78109
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-07-27 22:51:41 +1000

    first chat app using chatengine

---
+++ src/AuthPage.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const AuthPage = (props) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target[0];
+    axios
+      .post("http://localhost:800/authenticate", {
+        username: value,
+      })
+      .then((rep) => props.onAuth({ ...rep.data, secret: value }))
+      .catch((e) => e.message);
+  };
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Welcome 👋</div>
+
+        <div className="form-subtitle">Set a username to get started</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input className="auth-input" name="username" />
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
